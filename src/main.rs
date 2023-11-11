5d37622d58032c570577ff8c44e9c548f0937507
Scott Haiden <scott.b.haiden@gmail.com>
2023-11-10 23:02:27 -0800

    Small cleanups.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::io::{BufWriter, Write};
+use std::io::{BufWriter, Stdout, Write};
 
 fn to_ind(i: usize) -> usize { return (i - 3) / 2; }
+
+fn print_prime(out: &mut BufWriter<Stdout>, prime: u32) {
+    out.write_all(&prime.to_ne_bytes()).expect("write failed");
+}
+
+fn mark_composites(nums: &mut Vec<bool>, prime: usize) {
+    const MAX: usize = u32::MAX as usize;
+
+    let square: usize = prime * prime;
+    let double: usize = prime + prime;
+
+    for i in (square..MAX).step_by(double) {
+        nums[to_ind(i)] = true;
+    }
+}
 
 fn main() {
     let mut out = BufWriter::new(std::io::stdout());
@@ -8,14 +23,12 @@ fn main() {
     let len = to_ind(u32::MAX as usize) + 1;
     let mut nums = vec![false; len];
 
-    for i in (3..u32::MAX as usize).step_by(2) {
+    const LOW: usize = 3usize;
+    const HIGH: usize = u32::MAX as usize;
+
+    for i in (LOW..HIGH).step_by(2) {
         if nums[to_ind(i)] { continue; }
-
-        let bytes = (i as u32).to_be_bytes();
-        out.write_all(&bytes).expect("write failed");
-
-        for j in ((i * i)..(u32::MAX as usize)).step_by(i * 2) {
-            nums[to_ind(j)] = true;
-        }
+        print_prime(&mut out, i as u32);
+        mark_composites(&mut nums, i);
     }
 }
